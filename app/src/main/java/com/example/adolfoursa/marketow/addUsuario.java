627262758e96a3b4d9a16bb7ed20fd0205876a20
Roger <brogerariel@gmail.com>
2018-11-29 15:52:26 -0600

    interfaz ingresar usuario
    -

---
+++ app/src/main/java/com/example/adolfoursa/marketow/addUsuario.java
@@ -0,0 +1,13 @@
+package com.example.adolfoursa.marketow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class addUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_usuario);
+    }
+}
