d9f6a663a75bfac2c9357bff25363ab6ed57ba30
Luis Espinoza <luis.manuel.espinoza.estrada@gmail.com>
2018-11-29 14:18:32 -0600

    Actualizacion
    ..

--- app/src/main/java/com/example/adolfoursa/marketow/Producto.java
+++ app/src/main/java/com/example/adolfoursa/marketow/Producto.java
@@ -9,5 +9,6 @@ public class Producto extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_producto);
+        getSupportActionBar().setTitle("Agregar Producto");
     }
 }
