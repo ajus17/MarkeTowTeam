51359a2c81084513e8d9c0f06a53a354ca9c9e09
ajus17 <32777266+ajus17@users.noreply.github.com>
2018-11-30 07:53:34 -0600

    segundo dia

--- app/src/main/java/com/example/adolfoursa/marketow/paneldecontrol.java
+++ app/src/main/java/com/example/adolfoursa/marketow/paneldecontrol.java
@@ -2,13 +2,29 @@ package com.example.adolfoursa.marketow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class paneldecontrol extends AppCompatActivity {
+
+    //atributos de la clase
+    Button btAddProd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paneldecontrol);
         getSupportActionBar().setTitle("Panel de Control");
+
+        //instancias de los elementos de la interfaz
+        btAddProd = findViewById(R.id.btnAnadeproducto);
+
+        //Evento clic del botón agregar producto
+        btAddProd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
